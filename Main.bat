aab05a49c2ec26b738a4954e635801b0029b4021
Trent Patterson <jtrent238@outlook.com>
2018-10-05 16:12:48 -0400

    1052018412

--- Main.bat
+++ Main.bat
@@ -1,6 +1,17 @@
 @echo off
 cls
+
+If NOT exist "Temp" (
+	mkdir "Temp"
+) 
+
+SET TEMP_PYTHONVERSION=%random%.temp
+
+python -V>>Temp\%TEMP_PYTHONVERSION%
+echo %TEMP_PYTHONVERSION%
+echo Current Python version: <Temp\%TEMP_PYTHONVERSION%
+
 ::pip install wget
-pip install raven --upgrade
-python Main.py
+::pip install raven --upgrade
+::python Main.py
 pause>nul
